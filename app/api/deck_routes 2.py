fc9bd47b1a010b62ebc64aad7948f372f5e040ec
ashleighctucker <ashleighctucker@gmail.com>
2021-12-15 10:46:08 -0800

    edit route

--- app/api/deck_routes 2.py
+++ app/api/deck_routes 2.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, request
 from app.models import Deck, db
-from app.forms import NewDeckForm, EditDeckForm
+from app.forms import NewDeckForm
 from .auth_routes import validation_errors_to_error_messages
 
 deck_routes = Blueprint('dekcs', __name__)
 
 
-@deck_routes.route('/', methods=["POST"])
+@deck_routes.route('/')
 def create_deck():
     form = NewDeckForm()
     form['csrf_token'].data = request.cookies['csrf_token']
@@ -16,20 +16,5 @@ def create_deck():
         db.session.add(newDeck)
         db.session.commit()
         return newDeck.to_dict()
-    else:
-        return {'errors': validation_errors_to_error_messages(form.errors)}, 400
-
-
-@deck_routes.route('/<int:id>/', methods=["PUT"])
-def edit_deck(id):
-    form = EditDeckForm()
-    deckToEdit = Deck.query.get(int(id))
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        deckToEdit.title = form.data['title']
-        deckToEdit.cover_photo_url = form.data['cover_photo_url']
-        deckToEdit.category_id = form.data['category_id']
-        db.session.commit()
-        return deckToEdit.to_dict()
     else:
         return {'errors': validation_errors_to_error_messages(form.errors)}, 400
