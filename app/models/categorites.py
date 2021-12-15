ab54bfa6bd1aa0eb469e3a6066d24b3141dbecf2
ashleighctucker <ashleighctucker@gmail.com>
2021-12-14 18:14:28 -0800

    added categories table

---
+++ app/models/categorites.py
@@ -0,0 +1,16 @@
+from .db import db
+
+
+class Category (db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), nullable=False, unique=True)
+    color_hex = db.Column(db.String(256), nullable=False, unique=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'color_hex': self.color_hex,
+        }
