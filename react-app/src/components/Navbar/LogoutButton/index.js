7280cfca0ef7d3660337fd9b0519abeb9612a992
ashleighctucker <ashleighctucker@gmail.com>
2021-12-17 15:24:33 -0800

    logout refactor

--- react-app/src/components/Navbar/LogoutButton/index.js
+++ react-app/src/components/Navbar/LogoutButton/index.js
@@ -8,7 +8,11 @@ const LogoutButton = () => {
     await dispatch(logout());
   };
 
-  return <button onClick={onLogout}>Logout</button>;
+  return (
+    <button id="nav-logout" className="nav-buttons" onClick={onLogout}>
+      Logout
+    </button>
+  );
 };
 
 export default LogoutButton;
