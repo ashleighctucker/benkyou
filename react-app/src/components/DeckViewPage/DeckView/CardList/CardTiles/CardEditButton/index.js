2027ffd5d87d737e044140ead13d9e58b68a234d
ashleighctucker <ashleighctucker@gmail.com>
2021-12-21 00:18:57 -0800

    styled button

--- react-app/src/components/DeckViewPage/DeckView/CardList/CardTiles/CardEditButton/index.js
+++ react-app/src/components/DeckViewPage/DeckView/CardList/CardTiles/CardEditButton/index.js
@@ -3,13 +3,17 @@ import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
 
 import { Modal } from '../../../../../../context/Modal';
 import EditCardForm from './EditCardForm';
+import './EditCardButton.css';
 
 const EditCardButton = ({ card }) => {
   const [showEditModal, setShowEditModal] = useState(false);
 
   return (
     <>
-      <button onClick={() => setShowEditModal(true)}>
+      <button
+        className="edit-card-button"
+        onClick={() => setShowEditModal(true)}
+      >
         <EditTwoToneIcon />
         Edit Card
       </button>
