2c2d21c7065ebf41bf4b4d776cc9eae7681c18ed
ashleighctucker <ashleighctucker@gmail.com>
2022-02-03 10:30:31 -0800

    seed changes for collections

--- app/seeds/collections.py
+++ app/seeds/collections.py
@@ -2,13 +2,13 @@ from app.models import db, Collection, AddedDeck
 
 
 def seed_collections():
-    collection_1 = Collection(title="My Starter Decks",
+    collection_1 = Collection(title="My Starter Collection",
                               cover_photo_url="https://images.unsplash.com/photo-1639422704145-1264b281af8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80", has_image=True, user_id=1)
     add1 = AddedDeck(deck_id=1, collection_id=1)
     add2 = AddedDeck(deck_id=2, collection_id=1)
     add3 = AddedDeck(deck_id=3, collection_id=1)
 
-    collection_2 = Collection(title="Mixed Deck List",
+    collection_2 = Collection(title="Mixed Deck Collection",
                               cover_photo_url="https://images.unsplash.com/photo-1615483583516-d17ddb7862ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80", has_image=True, user_id=1)
     add4 = AddedDeck(deck_id=1, collection_id=2)
     add5 = AddedDeck(deck_id=2, collection_id=2)
